a98ac92b0fe6e1151bbf0e0e593123dbbf11d863
August Laulainen <august.laulainen@tuni.fi>
2022-08-31 14:40:12 +0300

    Finished temperature submission

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -7,6 +7,12 @@ int main()
     cout << "Enter a temperature: ";
 
     // Write your code here
+    
+    double temp;   //Adding temperature variable and reading it.
+    cin >> temp;
 
+    // Printing temperatures
+    cout << temp << " degrees Celsius is " << temp * 1.8 + 32 << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << (temp - 32)/1.8 << " degrees Celsius" << endl;
     return 0;
 }
