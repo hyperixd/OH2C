90bbb8c0f36e4ca069eb883795b3997fa3d98c62
August Laulainen <august.laulainen@tuni.fi>
2022-08-31 15:44:29 +0300

    Lisätty 3:lla jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,15 +6,23 @@ int main()
     std::cout << "How many numbers would you like to have? ";
 
     // Write your code here
+
     // Variable that stores how many numbers will be printed.
     int game_lft;
     // Variable that stores current game number.
     int game_num = 1;
     std::cin >> game_lft;
 
-    while(game_lft >= 1)
+    while(game_lft >= 1)    // Printing numbers when games left is bigger that 0
     {
-        std::cout << game_num << std::endl;
+        if(game_num % 3 == 0)   // If our number is divisible with 3, print "zip"
+        {
+            std::cout << "zip" << std::endl;
+            game_num += 1;
+            game_lft -= 1;
+            continue;
+        }
+        std::cout << game_num << std::endl; //Printing game number.
         game_num += 1;
         game_lft -= 1;
     }
