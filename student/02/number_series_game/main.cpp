2b7f0d3a862b8a82bcc667c28fe5c9dec2ab41dc
August Laulainen <august.laulainen@tuni.fi>
2022-08-31 15:32:27 +0300

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,6 +6,18 @@ int main()
     std::cout << "How many numbers would you like to have? ";
 
     // Write your code here
+    // Variable that stores how many numbers will be printed.
+    int game_lft;
+    // Variable that stores current game number.
+    int game_num = 1;
+    std::cin >> game_lft;
+
+    while(game_lft >= 1)
+    {
+        std::cout << game_num << std::endl;
+        game_num += 1;
+        game_lft -= 1;
+    }
 
     return 0;
 }
