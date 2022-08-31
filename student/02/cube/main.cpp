4ae1c7ef830f135f00782a91fbe15b77f7aadde0
August Laulainen <august.laulainen@tuni.fi>
2022-08-31 16:58:33 +0300

    Fixed cube submission

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -13,7 +13,7 @@ int main()
     // pow_num = selected number cubed
     int pow_num = num * num * num;
 
-    if(pow_num <= num)  // If cubed number is smaller than the original number, there has been integer overflow.
+    if(pow_num < num)  // If cubed number is smaller than the original number, there has been integer overflow.
     {
         cout << "Error! The cube of " << num << " is not " << pow_num << "." << endl;
 
