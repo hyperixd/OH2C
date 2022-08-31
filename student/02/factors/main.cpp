0b6b66eb0e1bd61b3332473936f12d5887d3f3e2
August Laulainen <august.laulainen@tuni.fi>
2022-08-31 16:29:39 +0300

    Finished factors submission

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -8,5 +9,21 @@ int main()
 
     // Write your code here
 
-    return 0;
+    int num;   // Declaring positive number variable.
+    cin >> num;
+    if(num <= 0)    // You can't factor zero so program will stop here.
+    {
+        cout << "Only positive numbers accepted" << endl;
+        return 0;
+    }
+
+    for(int factor = round(sqrt(num)); factor >= 1; factor--)
+    {
+        if(num % factor == 0)
+        {
+            cout << num << " = " << factor << " * " << num / factor << endl;
+            return 0;
+        }
+    }
+
 }
