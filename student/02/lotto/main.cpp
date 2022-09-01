f0d1bbe1bda32135c58f68d0fc7063a5ae8f6c78
August Laulainen <august.laulainen@tuni.fi>
2022-09-01 17:20:42 +0300

    Finished lotto submission

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+unsigned long long int fact(int luku)   // Calculating the factorials
+{   
+    unsigned long long int pal_luku = 1;
+
+    for(int i = 1; i <= luku; i++)
+    {
+        pal_luku *= i;
+    }
+
+    return pal_luku;
+}
+
+unsigned long long int lasku(int ball_total, int ball_draw) // Calculating total possibilities
+{
+    unsigned long long int vastaus = fact(ball_total) / (fact(ball_total - ball_draw) * fact(ball_draw));
+    return vastaus;
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int ball_total; // Number of total balls
+    cout << "Enter the total number of lottery balls: ";
+    cin >> ball_total;
+
+    int ball_draw;  // Number of balls that are drawn
+    cout << "Enter the number of drawn balls: ";
+    cin >> ball_draw;
+
+    if(ball_total < 1  || ball_draw < 1)
+    {
+        cout << "The number of balls must be a positive number." << endl;
+    }
+
+    else if(ball_draw > ball_total)
+    {
+        cout << "The maximum number of drawn balls is the total amount of balls." << endl;
+    }
+
+    else
+    {
+        cout << "The probability of guessing all " << ball_draw << " balls correctly is 1/" << lasku(ball_total, ball_draw) << endl;
+    }
+    
+    return EXIT_SUCCESS;
 }
