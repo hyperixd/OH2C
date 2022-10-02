6563ece297a60e60b3f86b89dca8b53eb3418934
August Laulainen <august.laulainen@tuni.fi>
2022-10-03 01:44:48 +0300

    Finished split

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -2,9 +2,43 @@
 #include <string>
 #include <vector>
 
+using namespace std;
 
 // TODO: Implement split function here
 // Do not change main function
+std::vector<std::string> split(std::string line, char merkki, bool empty_space = false)
+{
+    string::size_type paikka;
+    string::size_type last_paikka = 0;
+    vector<string> pilkottu;
+    while(line.find(merkki) != string::npos)
+    {
+        if(line.find(merkki) == 0 && empty_space == true)
+        {
+            line.erase(0,1);
+        }
+        else if(line.find(merkki) == 0 && empty_space == false)
+        {
+            pilkottu.push_back("");
+            line.erase(0,1);
+        }
+        else
+        {
+        
+        paikka = line.find(merkki);
+        pilkottu.push_back(line.substr(last_paikka, paikka));
+        line.erase(last_paikka, paikka + 1);
+        
+        }
+
+    }
+    if(line.size() != string::npos)
+    {
+        return pilkottu;
+    }
+    pilkottu.push_back(line);
+    return pilkottu;
+}
 
 
 int main()
