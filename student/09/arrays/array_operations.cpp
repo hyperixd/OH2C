3bdaf2e376bfd82f30202e03cee3b7c22a71388a
August Laulainen <august.laulainen@tuni.fi>
2022-11-16 18:51:45 +0200

    Finished arrays

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -1,1 +1,52 @@
 #include "array_operations.hh"
+
+int greatest_v1(int *itemptr, int size)
+{
+    int test = 1;
+    while(test < size)
+    {
+        itemptr++;
+        test++;
+    }
+    return *itemptr;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    endptr-=1;
+    return *endptr;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while(itemptr != endptr)
+    {
+        *targetptr = *itemptr;
+        itemptr++;
+        targetptr++;
+    }
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+
+    int x;
+    rightptr--;
+    while(leftptr != rightptr)
+    {
+        if(leftptr + 1 == rightptr)
+        {
+            *leftptr = *rightptr;
+            break;
+        }
+        else
+        {
+            x = *leftptr;
+            *leftptr = *rightptr;
+            *rightptr = x;
+            leftptr++;
+            rightptr--;
+        }
+    }
+
+}
